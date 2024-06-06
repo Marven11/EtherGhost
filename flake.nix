bb6d77c031ca8a72b2e0e7271f8917ea09f9354f
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-06-06 15:31:22 +0800

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonPackages = pkgs.python3Packages;
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          myapp = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.myapp;
+        };
+
+        # Shell for app dependencies.
+        #
+        #     nix develop
+        #
+        # Use this shell for developing your app.
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.myapp ];
+          buildInputs = [
+            pythonPackages.notebook
+            pythonPackages.ipython
+            pythonPackages.pydantic
+            pythonPackages.fastapi
+            pythonPackages.requests
+            pythonPackages.pycryptodome
+            pythonPackages.sqlalchemy
+            pythonPackages.uvicorn
+            pythonPackages.sqlalchemy-utils
+            pythonPackages.httpx
+            pythonPackages.chardet
+          ];
+
+        };
+
+        # Shell for poetry.
+        #
+        #     nix develop .#poetry
+        #
+        # Use this shell for changes to pyproject.toml and poetry.lock.
+        devShells.poetry = pkgs.mkShell {
+          packages = [ pkgs.poetry ];
+        };
+      });
+}
