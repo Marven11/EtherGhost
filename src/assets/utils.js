a936a8603db96d749f4b08353d72cad0710cb389
Your Name <a@a.a>
2024-06-08 16:13:54 +0800

    Change current url

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -6,7 +6,7 @@ export function hello() {
 }
 
 export function getCurrentApiUrl() {
-  return "http://127.0.0.1:8000" // TODO: rewrite me
+  return window.location.origin 
 }
 
 export function doAssert(result, msg) {
