54e62c967125d2b5bada2b75d245ddc8b651db78
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-06-16 18:33:57 +0800

    Add frontend/ again

---
+++ frontend/src/assets/utils.js
@@ -0,0 +1,76 @@
+import axios from "axios"
+import { popupsRef } from "./store"
+
+export function hello() {
+  console.log("Hello, World!")
+}
+
+export async function joinPath(folder, entry) {
+  return await getDataOrPopupError("/utils/join_path", {
+    params: {
+      folder: folder,
+      entry: entry
+    }
+  })
+}
+
+export function getCurrentApiUrl() {
+  return window.location.origin.includes("5173") ? "http://127.0.0.1:8022" : window.location.origin
+}
+
+export function doAssert(result, msg) {
+  if (result) {
+    return
+  }
+  if (msg) {
+    throw Error(msg)
+  } else {
+    throw Error("Assertion failed, message is not provided")
+  }
+}
+
+export function addPopup(color, title, msg) {
+  popupsRef.value.addPopup(color, title, msg)
+}
+
+export function parseDataOrPopupError(resp) {
+  if (resp.data.code != 0) {
+    let title = `未知错误：${resp.data.code}`
+    if (resp.data.code == -400) {
+      title = "客户端错误"
+    } else if (resp.data.code == -500) {
+      title = "服务端错误"
+    }
+    addPopup("red", title, resp.data.msg)
+    doAssert(false, `${title}: ${resp.data.msg}`)
+    return;
+  }
+  return resp.data.data
+}
+
+
+export async function getDataOrPopupError(uri, config) {
+  let url = `${getCurrentApiUrl()}${uri}`
+  let resp
+  try {
+    resp = await axios.get(url, config)
+  } catch (e) {
+    addPopup("red", "请求服务端失败", `无法请求${uri}，服务端是否正在运行？`)
+    throw e
+  }
+  console.log(resp)
+  return parseDataOrPopupError(resp, popupsRef)
+}
+
+export async function postDataOrPopupError(uri, config) {
+  let url = `${getCurrentApiUrl()}${uri}`
+  let resp
+  try {
+    resp = await axios.post(url, config)
+  } catch (e) {
+    addPopup("red", "请求服务端失败", `无法请求${uri}，服务端是否正在运行？`)
+    throw e
+  }
+  return parseDataOrPopupError(resp, popupsRef)
+}
+
---
+++ frontend/src/components/TerminalMain.vue
@@ -0,0 +1,127 @@
+<script setup>
+import { getDataOrPopupError, parseDataOrPopupError } from "@/assets/utils";
+import IconRun from "./icons/iconRun.vue"
+import { ref } from "vue";
+import Axios from "axios";
+import { getCurrentApiUrl } from "@/assets/utils";
+import { store } from "@/assets/store";
+import InputBox from "./InputBox.vue"
+
+
+const props = defineProps({
+  session: String,
+})
+const terminalInput = ref("")
+const terminalOutput = ref("")
+
+if (props.session) {
+  store.session = props.session
+}
+
+function addOutput(command, output) {
+  let leading = ""
+  if (terminalOutput.value) {
+    leading = `${terminalOutput.value}\n`;
+  }
+  terminalOutput.value = `${leading}$ ${command}\n${output}`
+  // change scroll position after text rendered.
+  setTimeout(() => {
+    let textarea = document.getElementById("command-output");
+    textarea.scrollTop = textarea.scrollHeight;
+  }, 0)
+}
+
+async function onExecuteCommand(event) {
+  const cmd = terminalInput.value;
+  event.preventDefault()
+  terminalInput.value = ""
+  const result = await getDataOrPopupError(`/session/${props.session}/execute_cmd`, {
+    params: {
+      cmd: cmd
+    }
+  })
+  addOutput(cmd, result)
+}
+
+// ###########
+// --- Input Box ---
+// ###########
+
+const showInputBox = ref(false)
+
+</script>
+
+<template>
+  <form action="" class="command-input" @submit="onExecuteCommand">
+    <input id="command-input" type="text" placeholder="cat /etc/passwd" v-model="terminalInput">
+    <div class="icon-run" @click="onExecuteCommand">
+      <IconRun />
+    </div>
+  </form>
+  <div class="terminal-output">
+    <textarea name="command-output" id="command-output" readonly :value="terminalOutput"></textarea>
+  </div>
+  <transition>
+    <InputBox v-if="showInputBox" title="测试标题" note="测试测试，这是一个测试" :requireInput="true"
+      @result="(_) => showInputBox = false" />
+  </transition>
+</template>
+
+<style scoped>
+.command-input {
+  display: flex;
+  height: 60px;
+}
+
+.command-input input {
+  background-color: var(--background-color-2);
+  color: var(--font-color-white);
+  border: none;
+  border-radius: 20px;
+  margin-right: 20px;
+  outline: none;
+  flex-grow: 1;
+  font-size: 24px;
+  text-indent: 10px;
+}
+
+.icon-run {
+  height: 60px;
+  width: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--background-color-2);
+  border-radius: 20px;
+  transition: all 0.3s ease;
+  opacity: 1;
+}
+
+.icon-run:hover {
+  background-color: var(--background-color-3);
+  outline: 2px solid var(--font-color-grey);
+}
+
+.terminal-output {
+  margin-top: 30px;
+  height: 85%;
+}
+
+.terminal-output textarea {
+  width: 100%;
+  height: 100%;
+  background-color: #00000015;
+  color: var(--font-color-white);
+  border-radius: 20px;
+  font-size: 24px;
+  padding: 20px;
+  outline: none;
+  border: none;
+  resize: none;
+}
+
+svg {
+  width: 35px;
+  stroke: var(--font-color-white);
+}
+</style>
