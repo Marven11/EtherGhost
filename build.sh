cd1bb0c24eac543deec3940c0a4fcecf19da6fc2
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-06-16 18:43:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 cd frontend/
 npm run build
-<<<<<<< HEAD
-=======
 rm ../guiren/public -rf
->>>>>>> 7cb998c (Make build.sh, we build frontend before building backend.)
 mv dist ../guiren/public
 cd ../guiren
 # TODO: build guiren packages
