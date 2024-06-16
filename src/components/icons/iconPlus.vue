15ea30b78e7fc9edf6581ddb42df7dde2772b319
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-06-16 16:41:07 +0800

    move frontend into frontend/ folder

--- src/components/icons/iconPlus.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6 12H18M12 6V18" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-    </svg>
-</template>
