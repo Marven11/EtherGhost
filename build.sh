8289c9666967dd1ebaceb141c05c8f14b7981cd3
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-06-16 16:48:07 +0800

    Make build.sh, we build frontend before building backend.

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 cd frontend/
 npm run build
+<<<<<<< HEAD
+=======
+rm ../guiren/public -rf
+>>>>>>> 7cb998c (Make build.sh, we build frontend before building backend.)
 mv dist ../guiren/public
 cd ../guiren
 # TODO: build guiren packages
