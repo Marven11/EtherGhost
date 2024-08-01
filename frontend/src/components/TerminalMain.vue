949f255452e4e867fe14a1d5f032aeb6fc805103
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-08-01 12:31:35 +0800

    New terminal function

--- frontend/src/components/TerminalMain.vue
+++ frontend/src/components/TerminalMain.vue
@@ -1,43 +1,88 @@
 <script setup>
-import Terminal from 'vue-web-terminal';
+import { store } from '@/assets/store';
+import { getDataOrPopupError } from '@/assets/utils';
+import { ref } from 'vue';
+import Terminal, { TerminalApi } from 'vue-web-terminal';
+
+const props = defineProps({
+  session: String,
+})
 
 const dragConf = {
-    width: "90%",
-    height: "80%",
-    zIndex: 100,
+  width: "90%",
+  height: "80%",
+  zIndex: 100,
 
-    pinned: false
+  pinned: false
 }
 
-function onExecCmd(key, command, success, failed) {
-    if (key === 'fail') {
-        failed('Something wrong!!!')
-    } else {
-        let allClass = ['success', 'error', 'system', 'info', 'warning'];
+if (props.session) {
+  store.session = props.session
+}
 
-        let clazz = allClass[Math.floor(Math.random() * allClass.length)];
-        success({
-            type: 'normal',
-            class: clazz,
-            tag: clazz,
-            content: `Your command is '${command}'`
-        })
+const pwd = ref("/unknown_pwd")
+
+function randomString(length) {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+  return result;
+}
+
+
+async function psudoExec(command) {
+  const result_text = await getDataOrPopupError(`/session/${props.session}/execute_cmd`, {
+    params: {
+      cmd: `cd ${pwd.value}; (${command}) 2>&1`
     }
+  })
+  return result_text
 }
+
+async function onExecCmd(key, command, success, failed) {
+  if (key == "cd") {
+    const result = await psudoExec(command)
+    // when `cd` success it produces no output
+    if (result.trim() == "") {
+      pwd.value = command.substring(2).trim()
+    }
+    success({
+      type: "ansi",
+      content: result
+    })
+  } else if (key == "clear") {
+    TerminalApi.clearLog("my-terminal")
+    success()
+  } else {
+    success({
+      type: 'ansi',
+      content: await psudoExec(command)
+    })
+  }
+}
+
+setTimeout(async () => {
+  const result = await getDataOrPopupError(`/session/${props.session}/execute_cmd`, {
+    params: {
+      cmd: "pwd"
+    }
+  })
+  pwd.value = result.trim()
+}, 0)
 </script>
 
 <template>
-    <div class="terminal-main">
-        <terminal name="my-terminal" :show-header="false" :log-size-limit="500" :enable-default-command="false"
-        @exec-cmd="onExecCmd" :drag-conf="dragConf" />
-    </div>
+  <div class="terminal-main">
+    <terminal name="my-terminal" :context="pwd" :show-header="false" :log-size-limit="500"
+      :enable-default-command="false" @exec-cmd="onExecCmd" :drag-conf="dragConf" />
+  </div>
 
 </template>
 
 <style scoped>
 .terminal-main {
-    height: 100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 }
-
 </style>
---
+++ frontend/src/components/icons/iconHash.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10 4L7 20M17 4L14 20M5 8H20M4 16H19" stroke-width="2" stroke-linecap="round" />
+    </svg>
+</template>
