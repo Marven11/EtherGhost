d0ec4f59bf06b83eb1ed2b34fd958987b6bb08e1
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-08-16 16:20:37 +0800

    Open browser on start

--- ether_ghost/__main__.py
+++ ether_ghost/__main__.py
@@ -1,8 +1,43 @@
+import argparse
+import webbrowser
+import threading
+import time
+
 from .main import app
+
+
+def open_browser(url):
+    def open_browser_thread():
+        time.sleep(0.2)
+        try:
+            webbrowser.open(url)
+        except webbrowser.Error:
+            print("Open browser failed")
+
+    t = threading.Thread(target=open_browser_thread)
+    t.daemon = True
+    t.start()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Example program")
+
+    parser.add_argument("--host", help="Host to connect to", default="127.0.0.1")
+    parser.add_argument("--port", type=int, help="Port to use", default=8022)
+    parser.add_argument("--no-browser", action="store_true", help="Do not open browser")
+
+    return parser.parse_args()
+
 
 def main():
 
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8022)
+
+    args = parse_args()
+    if not args.no_browser:
+        open_browser(f"http://{args.host}:{args.port}")
+    uvicorn.run(app, host=args.host, port=args.port)
+
+
 if __name__ == "__main__":
     main()
