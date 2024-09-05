90e23cc792c5e2f18993d2bfb0bd408baccf24d6
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-09-06 00:05:58 +0800

    Bugfix: xss for phpinfo

--- frontend/src/components/pages/BasicInfoMain.vue
+++ frontend/src/components/pages/BasicInfoMain.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { store } from '@/assets/store';
-import { getCurrentApiUrl, getDataOrPopupError } from '@/assets/utils';
+import { addPopup, getCurrentApiUrl, getDataOrPopupError } from '@/assets/utils';
 import { ref } from 'vue';
 import axios from 'axios';
+import sanitizeHtml from 'sanitize-html';
 
 const props = defineProps({
   session: String,
@@ -17,9 +18,38 @@ const infos = ref([
 ])
 
 async function openPhpInfo() {
-  const result = await axios.get(`${getCurrentApiUrl()}/session/${props.session}/download_phpinfo`, { responseType: 'blob' });
-  const url = URL.createObjectURL(result.data);
-  console.log(url)
+  addPopup("yellow", "显示可能有问题", "为了阻止XSS漏洞，打开的phpinfo html会被清理，导致某些元素显示不正常")
+  const result = await axios.get(`${getCurrentApiUrl()}/session/${props.session}/download_phpinfo`);
+  const style = `
+  <style type="text/css">
+body {background-color: #fff; color: #222; font-family: sans-serif;}
+pre {margin: 0; font-family: monospace;}
+a:link {color: #009; text-decoration: none; background-color: #fff;}
+a:hover {text-decoration: underline;}
+table {border-collapse: collapse; border: 0; width: 934px; box-shadow: 1px 2px 3px #ccc;}
+.center {text-align: center;}
+.center table {margin: 1em auto; text-align: left;}
+.center th {text-align: center !important;}
+td, th {border: 1px solid #666; font-size: 75%; vertical-align: baseline; padding: 4px 5px;}
+th {position: sticky; top: 0; background: inherit;}
+h1 {font-size: 150%;}
+h2 {font-size: 125%;}
+.p {text-align: left;}
+.e {background-color: #ccf; width: 300px; font-weight: bold;}
+.h {background-color: #99c; font-weight: bold;}
+.v {background-color: #ddd; max-width: 300px; overflow-x: auto; word-wrap: break-word;}
+.v i {color: #999;}
+img {float: right; border: 0;}
+hr {width: 934px; background-color: #ccc; border: 0; height: 1px;}
+</style>
+  `
+  const url = URL.createObjectURL(new Blob([style + sanitizeHtml(result.data, {
+    allowedAttributes: {
+      'div': ['class'],
+      'td': ['class'],
+      'tr': ['class'],
+    },
+  })], { type: 'text/html' }));
   window.open(url, "_blank")
 }
 
