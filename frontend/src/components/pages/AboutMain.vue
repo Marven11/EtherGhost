bd3a307c80ddc7e85db069ca50d4d949b2926750
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-10-11 23:09:29 +0800

    Check update button

--- frontend/src/components/pages/AboutMain.vue
+++ frontend/src/components/pages/AboutMain.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { getCurrentApiUrl, getDataOrPopupError } from '@/assets/utils';
+import { addPopup, getCurrentApiUrl, getDataOrPopupError } from '@/assets/utils';
 import { ref } from 'vue';
 
 const version = ref("Unknown Version")
@@ -8,6 +8,18 @@ setTimeout(async () => {
   let serverVersion = await getDataOrPopupError("/utils/version")
   version.value = `v${serverVersion}`
 }, 0)
+
+async function checkUpdate() {
+  let updateCheckInfo = await getDataOrPopupError("/utils/check_update")
+  console.log(updateCheckInfo)
+  // Object { has_new_version: false, current_version: "0.1.0", new_version: "0.1.0" }
+  if (!updateCheckInfo.has_new_version) {
+    addPopup("green", "当前没有更新", `当前版本${updateCheckInfo.current_version}是最新版本`)
+  } else {
+    addPopup("blue", "检测到新版！", `目前的最新版本是${updateCheckInfo.new_version}，当前版本${updateCheckInfo.current_version}`)
+  }
+}
+
 
 </script>
 
@@ -21,6 +33,10 @@ setTimeout(async () => {
         <a href="https://github.com/Marven11/EtherGhost">Github</a>
         | <a href="https://github.com/Marven11/EtherGhost/issues">漏洞报告与功能建议</a>
       </p>
+      <div class="actions">
+        <div class="button" @click="checkUpdate">检查更新</div>
+
+      </div>
     </div>
 
   </div>
@@ -41,7 +57,7 @@ setTimeout(async () => {
   width: 30%;
   border-radius: 20px;
   color: var(--font-color-secondary);
-  padding: 20px;
+  padding: 2rem;
 }
 
 .info-panel h1 {
@@ -52,5 +68,9 @@ setTimeout(async () => {
 .info-value {
   font-weight: bold;
   color: var(--font-color-primary);
+}
+
+.actions {
+  width: max-content;
 }
 </style>
