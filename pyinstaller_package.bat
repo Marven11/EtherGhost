790ef3a85d93b9704da41719e27d8fba3bf4df71
Marven11 <110723864+Marven11@users.noreply.github.com>
2025-03-26 21:23:42 +0800

    Update pyinstaller file

--- pyinstaller_package.bat
+++ pyinstaller_package.bat
@@ -1,6 +1,6 @@
-pyinstaller -p C:\\Users\\user\\AppData\\Local\\pypoetry\\Cache\\virtualenvs\\ether-ghost-wKJVcvsy-py3.12\\Lib\\site-packages ^
-  --add-data ether_ghost\utils\eng_words_map.json:ether_ghost\utils ^
+pyinstaller --add-data ether_ghost\utils\eng_words_map.json:ether_ghost\utils ^
   --add-data ether_ghost\utils\phone_5.json:ether_ghost\utils ^
+  --add-data ether_ghost\sessions\Payload.java:ether_ghost\sessions ^
   --add-data ether_ghost\public:ether_ghost\public ^
   --add-data ether_ghost\vessel_php\client.php:ether_ghost\vessel_php ^
   --add-data ether_ghost\vessel_php\server.php:ether_ghost\vessel_php ^
